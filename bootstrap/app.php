3cbd0c90ecd5a92295dc7a0f56bdb98561e33575
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-03-11 12:03:44 -0300

    Tradução da aplicação implementada

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Overtrue\LaravelLang\TranslationServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
